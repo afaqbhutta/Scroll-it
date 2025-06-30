7e51bb1fdc84a2620d758f468c64d9748ee67d1f
AFAQ HUSSAIN <161577646+afaqbhutta@users.noreply.github.com>
2025-06-30 21:33:27 +0000

    landing page

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import flask from flask
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+entries = []  # This will be lost when the app restarts
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        movie = request.form['movie']
+        entries.append({'name': name, 'movie': movie})
+    return render_template('home.html', entries=entries)
+
+if __name__ == '__main__':
+    app.run(debug=True)
